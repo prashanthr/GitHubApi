3d88a265d3e7f799bec08326d8179029c6ef6ffc
Prashanth Rajaram <prashanthrdev@gmail.com>
2016-06-12 16:53:05 -0500

    Adding constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+module.exports = function Constants {
+	API_ROOT: "https://api.github.com",
+	REPOS: "repos"
+}
+//Sample Requests
+//https://api.github.com/users/[username]/repos
+//https://api.github.com/users/[username]
+//https://api.github.com/users/[username]/orgs
